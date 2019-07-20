27e52cf58f61ecec81f68f80fa974a8176b78143
The-Broccoli <korepp.f@gmail.com>
2019-07-20 23:56:32 +0200

    first steps restructuring config.py

--- logon/src/database/Config.py
+++ logon/src/database/Config.py
@@ -28,6 +28,11 @@ class Config:
 
     def createDefault(self):
         try:
+            self.config['LogonServer Basic Settings'] = {
+            'Logon_Server_Ip': '127.0.0.1',
+            'Logon_Server_Port': '478',
+            'Logon_Server_Version': '1.29.1'
+            }
             self.config['LogonServer Database'] = {
             'logon_database_ip': '127.0.0.1',
             'logon_database_port': '3306',
@@ -35,13 +40,47 @@ class Config:
             'logon_database_user': 'root',
             'logon_database_pass': 'fabio312'
             }
+            self.config['RealmList Configuration'] = {
+            'Network_RealmList_Ip': '127.0.0.1',
+            'Network_RealmList_Port': '451'
+            }
             with open('./logon.conf', 'w') as configfile:    # save
                 self.config.write(configfile)
             return True
         except:
             return False
 
-# Config().createDefault()
+# ======================================================================
+
+    # self.config = configparser.ConfigParser()
+
+    def initialize(self):
+        try:
+            section01 = 'LogonServer Database'
+            section02 = 'Network RealmList'
+            section03 = 'Logon Server'
+            config.read('./logon.conf')
+
+            self.ip = self.config.get(section01, 'logon_database_ip')
+            self.port = self.config.getint(section01, 'logon_database_port')
+            self.name = self.config.get(section01, 'logon_database_name')
+            self.user = self.config.get(section01, 'logon_database_user')
+            self.passwo = self.config.get(section01, 'logon_database_pass')
+
+            self.exchangeIp = self.config.get(section02, 'Network_RealmList_Ip')
+            self.exchangePort = self.config.get(section02, 'Network_RealmList_Port')
+
+            self.loginIp = (section03, 'Logon_Server_Ip')
+            self.loginPort = (section03, 'Logon_Server_Port')
+            self.version = (section03, 'Logon_Server_Version')
+        except:
+            print('[ERROR] Config.initialize: Unreadable config or missing fields')
+    
+
+
+
+
+Config().createDefault()
 # DbConfig = Config()
 # print(type(DbConfig.get()))
 # print(DbConfig.get())
