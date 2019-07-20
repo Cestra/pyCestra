3fd04307a57f77abb9c1516e33eb9af36d21fe02
The-Broccoli <korepp.f@gmail.com>
2019-07-20 21:00:43 +0200

    definition of methods in object.Account

--- logon/src/object/Account.py
+++ logon/src/object/Account.py
@@ -1,2 +1,76 @@
 class Account():
-    pass
+
+    def Account(self):
+        pass
+
+    def getID(self):
+        pass
+    
+    def setID(self):
+        pass
+    
+    def getName(self):
+        pass
+    
+    def setName(self):
+        pass
+    
+    def getName(self):
+        pass
+    
+    def setNickName(self):
+        pass
+    
+    def getNickName(self):
+        pass
+
+    def setQuestion(self):
+        pass
+    
+    def getQuestion(self):
+        pass
+    
+    def isStaff(self):
+        pass
+
+    def getClient(self):
+        pass
+    
+    def getState(self):
+        pass
+
+    def setState(self):
+        pass
+
+    def getServer(self):
+        pass
+
+    def setServer(self):
+        pass
+
+    def getgetSubscribeRemaining(self):
+        pass
+
+    def getSubscribe(self):
+        pass
+
+    def setSubscribe(self):
+        pass
+
+    def isSubscribes(self):
+        pass
+
+    def setBanned(self):
+        pass
+
+    def isBanned(self):
+        pass
+
+    def addPlayer(self):
+        pass
+
+    def delPlayer(self):
+        pass
+
+    def getPlayers(self):
+        pass
