12a3ac050b5eb68aeaeda9aab2961ce90fe50bf4
The-Broccoli <korepp.f@gmail.com>
2019-07-20 21:23:19 +0200

    definition of methods in object.Player and object.Server

--- logon/src/object/Player.py
+++ logon/src/object/Player.py
@@ -1,2 +1,19 @@
 class Player():
-    pass
+
+    def Player(self):
+        pass
+
+    def getGroupe(self):
+        pass
+
+    def getId(self):
+        pass
+
+    def setId(self):
+        pass
+
+    def getServer(self):
+        pass
+
+    def setServer(self):
+        pass
--- logon/src/object/Server.py
+++ logon/src/object/Server.py
@@ -1,2 +1,63 @@
 class Server():
-    pass
+
+    def Server(self):
+        pass
+
+    def getId(self):
+        pass
+
+    def setId(self):
+        pass
+
+    def isSubscribe(self):
+        pass
+
+    def getPort(self):
+        pass
+
+    def setPort(self):
+        pass
+
+    def getIp(self):
+        pass
+
+    def setIp(self):
+        pass
+
+    def getState(self):
+        pass
+
+    def setState(self):
+        pass
+
+    def getKey(self):
+        pass
+
+    def setKey(self):
+        pass
+
+    def getSub(self):
+        pass
+
+    def getClient(self):
+        pass
+
+    def setClient(self):
+        pass
+
+    def getFreePlaces(self):
+        pass
+
+    def setFreePlaces(self):
+        pass
+
+# public static Server get / logon/src/org/cestra/object/Server.java / line 112
+
+    def send(self):
+        pass
+
+    def sendHostListToAll(self):
+        pass
+
+    def getHostList(self):
+        pass
