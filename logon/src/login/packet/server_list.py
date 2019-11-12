1ff7c61265723f631843fa27a0e7ab36be355d81
The-Broccoli <korepp.f@gmail.com>
2019-11-13 00:14:36 +0100

    add server_list.py

---
+++ logon/src/login/packet/server_list.py
@@ -0,0 +1,24 @@
+from core.logging_handler import Logging
+
+
+class ServerList:
+    '''
+        AxK +
+        subscribe(in millisecond) +
+        server.getId() +  
+        number of characters
+
+    client.write('AxK2628000000|1,4')
+    '''
+    def __init__(self):
+        self.log = Logging()
+
+    def get_list(self, client):
+        client.write("AxK" + ServerList.server_list(client.get_account()))
+        pass
+
+    def server_list(account):
+        return '0|0,0'
+
+    def character_number(self):
+        pass
