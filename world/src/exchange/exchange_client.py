26497182b7aeb01a053c2294b1207dfb424bb803
The-Broccoli <korepp.f@gmail.com>
2019-12-04 01:56:45 +0100

    fun with exchange_client.py

--- world/src/exchange/exchange_client.py
+++ world/src/exchange/exchange_client.py
@@ -1,14 +1,27 @@
 import socket
+import time
+
+from core.logging_handler import Logging
+
 
 class ExchangeClient():
 
+    def __init__(self):
+        self.log = Logging
+
     def test(self):
-        HOST = '127.0.0.1'  # The server's hostname or IP address
-        PORT = 451        # The port used by the server
+        HOST = '127.0.0.1'
+        PORT = 451
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST, PORT))
-            s.sendall(b'Hello, world')
-            data = s.recv(1024)
-
-        print('Received', repr(data))
+        while True:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                s.connect((HOST, PORT))
+                print('wir sind dir')
+                s.send(b'Hello, world')
+                data = s.recv(1024)
+                print('Received', repr(data))
+            except:
+                s.close()
+                print('warten...')
+                time.sleep(2.5)
