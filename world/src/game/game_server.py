543602382f1f33bf540646e31c012d65a6e87e0d
The-Broccoli <korepp.f@gmail.com>
2019-12-11 01:11:32 +0100

    [world] add game_server.py and game_client.py
    work in progress, must sleep!

---
+++ world/src/game/game_server.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+
+from core.logging_handler import Logging
+
+
+class GameServer:
+
+    def __init__(self):
+        self.log = Logging()
+        self.log.warning('GameServer instans !')
+
+    def initialize(self, ip, port):
+        threadName = 'Game-Server - ' + str(port)
+        try:
+            t = threading.Thread(target=super.server,
+                                name=threadName,
+                                args=(self, ip, port))
+            t.start()
+        except:
+            self.log.warning('Game Server could not be created')
+    
+    def server(self, game_ip, game_port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.bind((game_ip, game_port))
+        except socket.error:
+            self.log.warning('Game Socket - Binding faild')
+        s.listen()
+        self.log.info('Game Socket is listening on Port: ' + str(game_port))
+        while True:
+            c, self.addr = s.accept()
+            self.log.info('Connected '+ str(self.addr[0])+ ':'+ str(self.addr[1]))
+            LoginHandler().session_created(c, self.addr)
+        s.close()
+
+    def session_created(self, soecket, addr):
+        threadName = 'Game-Client '+str(addr[0])+':'+ str(addr[1])
+        try:
+            t = threading.Thread(target=HelloExchangeClient,
+                                name=threadName,
+                                args=(soecket, addr,))
+            t.start()
+        except:
+            self.log.warning('Game Client could not be created '+ str(addr[0])+':'+ str(addr[1]))
