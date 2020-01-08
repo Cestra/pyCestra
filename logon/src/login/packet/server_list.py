d786e3627b2647e015afa0a7dda85fa715b721b1
The-Broccoli <korepp.f@gmail.com>
2020-01-09 00:17:41 +0100

    [logon] call error @ server_list

--- logon/src/login/packet/server_list.py
+++ logon/src/login/packet/server_list.py
@@ -14,10 +14,9 @@ class ServerList:
         self.log = Logging()
 
     def get_list(self, client):
-        client.write("AxK" + ServerList.server_list(client.get_account()))
-        pass
+        client.write("AxK" + ServerList().server_list(client.get_account()))
 
-    def server_list(account):
+    def server_list(self, account):
         return '0|0,0'
 
     def character_number(self):
