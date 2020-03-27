4d7bf4e9aae672e435064afc5ff0da11ba6bedeb
The-Broccoli <The-Broccoli@users.noreply.github.com>
2020-03-28 00:08:30 +0100

    embarrassing...

--- testo/testo.py
+++
@@ -1,8 +0,0 @@
-import os
-
-try:
-    path = os.path.abspath("./logon/src/main.py")
-    command = 'python ' + path
-    os.system(command)
-except:
-    print('LOGON MAIN.py kann nicht gestart werden')
