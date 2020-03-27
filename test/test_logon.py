436c4c0ee5aa11566ab20ddddebe818740e0de48
The-Broccoli <The-Broccoli@users.noreply.github.com>
2020-03-28 00:17:59 +0100

    [test] del test_samples.py

--- test/test_logon.py
+++ test/test_logon.py
@@ -1,44 +1,44 @@
-import unittest
-import os
-import threading
-import time
-import sys
+# import unittest
+# import os
+# import threading
+# import time
+# import sys
 
 
-class LogonServerDemo(object):
+# class LogonServerDemo(object):
 
-    def __init__(self):
-        print('DEMO START')
+#     def __init__(self):
+#         print('DEMO START')
 
-    def server(self):
-        t = threading.currentThread()
-        while getattr(t, "do_run", True):
-            try:
-                path = os.path.abspath("./logon/src/main.py")
-                command = 'python ' + path
-                os.system(command)
-            except:
-                print('LOGON MAIN.py kann nicht gestart werden')
-        print("Stopping as you wish.")
+#     def server(self):
+#         t = threading.currentThread()
+#         while getattr(t, "do_run", True):
+#             try:
+#                 path = os.path.abspath("./logon/src/main.py")
+#                 command = 'python ' + path
+#                 os.system(command)
+#             except:
+#                 print('LOGON MAIN.py kann nicht gestart werden')
+#         print("Stopping as you wish.")
 
-    def start(self):
-        threadName = 'DEMO-SERVER'
-        t = threading.Thread(target=LogonServerDemo.server,
-                            name=threadName,
-                            args=(self,))
-        t.start()
-        print
+#     def start(self):
+#         threadName = 'DEMO-SERVER'
+#         t = threading.Thread(target=LogonServerDemo.server,
+#                             name=threadName,
+#                             args=(self,))
+#         t.start()
+#         print
     
-    def stop(self):
-        sys.exit(0)
+#     def stop(self):
+#         sys.exit(0)
 
-class TestSamples(unittest.TestCase):
-    def test_01(self):
-        demo = LogonServerDemo()
-        x = demo.start()
-        time.sleep(10)
-        demo.stop()
-        self.assertEqual(x, 'ding')
+# class TestSamples(unittest.TestCase):
+#     def test_01(self):
+#         demo = LogonServerDemo()
+#         x = demo.start()
+#         time.sleep(10)
+#         demo.stop()
+#         self.assertEqual(x, 'ding')
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
