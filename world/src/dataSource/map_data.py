704542e0f26e8950c5ff35dce3ab8ac56b2db67d
The-Broccoli <The-Broccoli@users.noreply.github.com>
2020-04-04 14:18:47 +0200

    add license

--- world/src/dataSource/map_data.py
+++ world/src/dataSource/map_data.py
@@ -1,3 +1,21 @@
+'''
+pyCestra - Open Source MMO Framework
+Copyright (C) 2020 pyCestra Team
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import dataSource
 from core.logging_handler import Logging
 from dataSource.DAO import DAO
