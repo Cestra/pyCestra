309623c8f98d3c141cb0bdb1023a7056b734f709
The-Broccoli <The-Broccoli@users.noreply.github.com>
2020-04-21 15:47:33 +0200

    [test] the tests are now running in a loop

--- test/test_client.py
+++ test/test_client.py
@@ -26,29 +26,14 @@ class TestClient(unittest.TestCase):
     def test_01_normal_connection(self):
         demoClient = DemoClient01()
         self.result = demoClient.start('unittest-01', 'unittest-01')
-        self.assertEqual(self.result['test_connection'], True)
-        self.assertEqual(self.result['test_key_response'], True)
-        self.assertEqual(self.result['test_packet_01'], True)
-        self.assertEqual(self.result['test_packet_02'], True)
-        self.assertEqual(self.result['test_packet_03'], True)
-        self.assertEqual(self.result['test_packet_04'], True)
-        self.assertEqual(self.result['test_packet_05'], True)
-        self.assertEqual(self.result['test_packet_06'], True)
-        self.assertEqual(self.result['test_packet_07'], True)
+        for test_name, result in self.result.items():
+            self.assertTrue(result)
 
     def test_02_no_nickname_connection(self):
         demoClient = DemoClient02()
         self.result = demoClient.start('unittest-02', 'unittest-02')
-        self.assertEqual(self.result['test_connection'], True)
-        self.assertEqual(self.result['test_key_response'], True)
-        self.assertEqual(self.result['test_packet_00'], True)
-        self.assertEqual(self.result['test_packet_01'], True)
-        self.assertEqual(self.result['test_packet_02'], True)
-        self.assertEqual(self.result['test_packet_03'], True)
-        self.assertEqual(self.result['test_packet_04'], True)
-        self.assertEqual(self.result['test_packet_05'], True)
-        self.assertEqual(self.result['test_packet_06'], True)
-        self.assertEqual(self.result['test_packet_07'], True)
+        for test_name, result in self.result.items():
+            self.assertTrue(result)
 
 if __name__ == '__main__':
     unittest.main()
