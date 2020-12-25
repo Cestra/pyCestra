4d20c348a867ca3a25533f36015dc9a0a6835456
The-Broccoli <The-Broccoli@users.noreply.github.com>
2020-12-25 03:56:18 +0100

    Update main.py

--- logon/src/main.py
+++ logon/src/main.py
@@ -35,12 +35,12 @@ def main():
     console = Console()
     console.clear()
 
-    def wel():
-        welmsg = [58*'─', '|  0.01  |' + 12*' ' + 
-                'pyCestra - Logon Server'+ 12*' ' + '|', 58*"─"]
-        for x in welmsg:
-            print(bcolors.blue + x + bcolors.cend)
-    wel()
+    # def wel():
+    #     welmsg = [58*'─', '|  0.01  |' + 12*' ' + 
+    #             'pyCestra - Logon Server'+ 12*' ' + '|', 58*"─"]
+    #     for x in welmsg:
+    #         print(bcolors.blue + x + bcolors.cend)
+    # wel()
 
     # ======================================================
     # preload data
